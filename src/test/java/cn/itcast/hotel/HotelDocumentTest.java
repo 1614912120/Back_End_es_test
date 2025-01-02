2535e0cb465f02de4454f6ad887d753062ea74e6
ryj <1614912120@qq.com>
2025-01-02 12:16:55 +0800

    查询 删除 更新 批量 操作文档

--- src/test/java/cn/itcast/hotel/HotelDocumentTest.java
+++ src/test/java/cn/itcast/hotel/HotelDocumentTest.java
@@ -6,7 +6,12 @@ import cn.itcast.hotel.service.IHotelService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -18,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest
 public class HotelDocumentTest {
@@ -50,5 +56,53 @@ public class HotelDocumentTest {
         // 2.准备Json文档
         request.source(json, XContentType.JSON);
         client.index(request,RequestOptions.DEFAULT);
+    }
+
+    //查询文档
+    @Test
+    void testGetDocumentById() throws IOException {
+        GetRequest request = new GetRequest("hotel", "61083");
+        //发送请求
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        String json = response.getSourceAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        HotelDoc hotelDoc = mapper.readValue(json, HotelDoc.class);
+        System.out.println(hotelDoc);
+    }
+
+    //删除文档
+    @Test
+    void testDeleteDocument() throws IOException {
+        DeleteRequest request = new DeleteRequest("hotel", "61083");
+        client.delete(request,RequestOptions.DEFAULT);
+    }
+
+    //增量修改
+    @Test
+    void testUpdateDocument() throws IOException {
+        UpdateRequest request = new UpdateRequest("hotel", "61083");
+        request.doc(
+                "price", "952",
+                "starName", "四钻"
+        );
+
+        client.update(request,RequestOptions.DEFAULT);
+    }
+
+    //批量导入文档
+    @Test
+    void testBulk() throws Exception {
+        List<Hotel> hotels = hotelService.list();
+        ObjectMapper mapper = new ObjectMapper();
+        //创建request
+        BulkRequest request = new BulkRequest();
+        for (Hotel hotel : hotels) {
+            HotelDoc hotelDoc = new HotelDoc(hotel);
+            //创建新增文档的request对象
+            request.add(new IndexRequest("hotel")
+                    .id(hotelDoc.getId().toString())
+                    .source(mapper.writeValueAsString(hotelDoc),XContentType.JSON));
+        }
+        client.bulk(request,RequestOptions.DEFAULT);
     }
 }
